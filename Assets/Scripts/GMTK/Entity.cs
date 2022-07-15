a5345025462df0b29cc3bbe99326f7ed84c99689
NocTua <noctua0609@gmail.com>
2022-07-15 22:54:38 +0200

    methode++

--- Assets/Scripts/GMTK/Entity.cs
+++ Assets/Scripts/GMTK/Entity.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public abstract class Entity : MonoBehaviour
 {
     public abstract void Play();
+
+    public bool IsDead()
+    {
+        if (health == 0)
+            return true;
+        return false;
+    }
     private int health;
     private int damage;
 }
