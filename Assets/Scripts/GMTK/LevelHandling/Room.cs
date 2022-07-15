e786d639cdf82c88255acf05d744bb4ec9359665
Erik Kubiak <erikkubiak@gmail.com>
2022-07-15 22:54:48 +0200

    current room work

---
+++ Assets/Scripts/GMTK/LevelHandling/Room.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GMTK.LevelHandling
+{
+    public class Room : MonoBehaviour
+    {
+        [SerializeField] private Transform m_Entry;
+        [SerializeField] private Transform m_Exit;
+
+        [SerializeField] private List<Enemies> m_Enemies;
+
+        public IEnumerable<Enemies> Enemies => m_Enemies;
+
+        public bool HasEnemies => true;
+    }
+}
