87e1ed67dd7c85099f935fda1cf504df4bc989ee
Erik Kubiak <erikkubiak@gmail.com>
2022-07-17 07:53:56 +0200

    Enemies HealthBar

--- Assets/Scripts/GMTK/Enemies.cs
+++ Assets/Scripts/GMTK/Enemies.cs
@@ -1,12 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using GMTK;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemies : Entity
 {
-    [SerializeField] private Player_holder player_holder;
+    [SerializeField] private Slider m_HealthBar;
     
+    [SerializeField] private Player_holder player_holder;
+
+
+    private void Start()
+    {
+        m_HealthBar.minValue = 0;
+        m_HealthBar.maxValue = health_max;
+    }
+
     public override void Play()
     {
         attack(player_holder.player);
@@ -17,6 +28,10 @@ public class Enemies : Entity
         health = health - damage_taken;
         if (health < 0)
             health = 0;
+
+        // TODO: Use a better way to do that
+        // Like having setter in entity that has a virtual method OnHealthChanged
+        m_HealthBar.value = health;
     }
 
     public void attack(Player player)
