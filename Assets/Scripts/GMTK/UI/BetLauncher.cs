47c3729fc971952cd7be36b509036377eeb8c5cb
Erik Kubiak <erikkubiak@gmail.com>
2022-07-17 07:31:23 +0200

    Update Bet Launcher to display die face

--- Assets/Scripts/GMTK/UI/BetLauncher.cs
+++ Assets/Scripts/GMTK/UI/BetLauncher.cs
@@ -4,15 +4,18 @@ using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 namespace GMTK.UI
 {
     public class BetLauncher : MonoBehaviour
     {
-        [SerializeField] private TextMeshProUGUI m_Text;
-        
-        
+        [SerializeField] private Image m_Image;
+
+        [SerializeField] private Sprite[] m_Faces;
+
+
         [SerializeField] private float m_BetDuration;
         [PropertyRange(0, "m_BetDuration")]
         [SerializeField] private float m_RefreshRate;
@@ -25,6 +28,7 @@ namespace GMTK.UI
 
         public IEnumerator c_Bet(Action<int> OnBetFinished)
         {
+            m_Image.gameObject.SetActive(true);
             int value;
             float time = 0;
             while (time < m_BetDuration)
@@ -38,11 +42,13 @@ namespace GMTK.UI
             value = Random.Range(1, 7);
             UpdateText(value);
             OnBetFinished(value);
+            m_Image.gameObject.SetActive(false);
+
         }
 
         private void UpdateText(int value)
         {
-            m_Text.text = value.ToString();
+            m_Image.sprite = m_Faces[value - 1];
         }
     }
 }
