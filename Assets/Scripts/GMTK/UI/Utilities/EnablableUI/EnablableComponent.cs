d9234b332a31cc81c5ef4d2f120cdd9bac61298a
Erik Kubiak <erikkubiak@gmail.com>
2022-07-17 17:13:53 +0200

    Fix UI being draggable even when not the turn

---
+++ Assets/Scripts/GMTK/UI/Utilities/EnablableUI/EnablableComponent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace GMTK.UI.Utilities.EnablableUI
+{
+    public class EnablableComponent : AEnablableUI
+    {
+        [SerializeField] private MonoBehaviour m_Component;
+        
+        
+        protected override void OnStateChanged(bool state)
+        {
+            m_Component.enabled = state;
+        }
+    }
+}
---
+++ Assets/Scripts/GMTK/UI/Utilities/EnablableUI/EnablableUIList.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace GMTK.UI.Utilities.EnablableUI
+{
+    public class EnablableUIList : AEnablableUI
+    {
+        [SerializeField] private AEnablableUI[] m_Enablable;
+        
+        
+        protected override void OnStateChanged(bool state)
+        {
+            foreach (var enablable in m_Enablable)
+            {
+                enablable.State = state;
+            }
+        }
+    }
+}
