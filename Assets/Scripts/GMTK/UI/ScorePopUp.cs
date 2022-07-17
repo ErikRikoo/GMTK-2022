b0df844a707881fa460b60b6ee9fbe34d6410af8
Erik Kubiak <erikkubiak@gmail.com>
2022-07-17 19:00:46 +0200

    Add victory Pop Up

---
+++ Assets/Scripts/GMTK/UI/ScorePopUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace GMTK.UI
+{
+    public class ScorePopUp : MonoBehaviour
+    {
+        [SerializeField] private string m_Format = "Score: {0}";
+        
+        [SerializeField] private TextMeshProUGUI m_Display;
+
+        [SerializeField] private Player_holder m_Player;
+        
+        private void OnEnable()
+        {
+            m_Display.text = String.Format(m_Format, m_Player.player.score);
+        }
+    }
+}
