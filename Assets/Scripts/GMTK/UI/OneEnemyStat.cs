138292ad908ec27301808c76d20e40abd9371905
Erik Kubiak <erikkubiak@gmail.com>
2022-07-17 12:37:25 +0200

    Add enemy stat stuff, needs tests

---
+++ Assets/Scripts/GMTK/UI/OneEnemyStat.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GMTK.UI
+{
+    public class OneEnemyStat : MonoBehaviour
+    {
+        [SerializeField] private GameObject m_IsDeadDisplay;
+        [SerializeField] private Image m_Icon;
+        [SerializeField] private TextMeshProUGUI m_DamageDisplay;
+        
+        public void UpdateDisplay(Enemies enemy)
+        {
+            m_DamageDisplay.text = enemy.damage.ToString();
+            m_IsDeadDisplay.SetActive(enemy.IsDead());
+            // TODO: Get Icon from enemy
+        }
+    }
+}
